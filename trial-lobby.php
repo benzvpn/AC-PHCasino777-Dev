970c1f8e6e858169ee9c4b3e5e69099c394393b3
TommySTYP <tommystyp@gmail.com>
2020-04-29 23:18:04 +0700

    commit

--- trial-lobby.php
+++ trial-lobby.php
@@ -8,7 +8,7 @@
 <body>
     <a href="/trial-sa" target="_blank">ทดลองเล่น SA Gaming</a><BR>
     <a href="/trial-se" target="_blank">ทดลองเล่น AE Gaming (Sexy Casino)</a><BR>
-    <a href="/trial-pg" target="_blank">ทดลองเล่น PG Soft</a><BR>
+    <a href="/trial-pg" target="_blank">ทดลองเล่น PG Soft</a>
 </body>
 
 </html>
