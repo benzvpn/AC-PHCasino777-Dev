b2a852bb6304b42884a3c428db655d6893cae0d5
TommySTYP <tommystyp@gmail.com>
2020-04-29 23:41:59 +0700

    commit

--- config/site.php
+++ config/site.php
@@ -2,7 +2,7 @@
 return array(
 	'site_id' => 'DEV01',
     'host' => 'http://ac-dev.myserver.local',
-	'app_dir' => 'C:\\xampp\\htdocs\\dev\\dev\\AC-PHCasino777-Dev',
+	'app_dir' => 'C:\\xampp\\htdocs\\dev\\AC-PHCasino777-Dev',
 	'img_url' => '/assets/images/',
 	'line_at_url' => 'https://line.me/R/ti/p/@allcasino777',
 	'css_url' => '/assets/css/',
